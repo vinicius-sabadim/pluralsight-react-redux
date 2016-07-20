1379399b3fc51ef0d550b59d4650ad4dca0db382
vinicius-sabadim <vinicius.sabadim@gmail.com>
2016-07-19 22:08:25 -0300

    Adding the courses page

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,11 +1,12 @@
 import React, { PropTypes } from 'react'
 import { Link, IndexLink } from 'react-router'
-// import LoadingDots from './LoadingDots'
 
 const Header = () => {
 	return (
 		<nav>
 			<IndexLink to="/" activeClassName="active">Home</IndexLink>
+			{" | "}
+			<Link to="/courses" activeClassName="active">Courses</Link>
 			{" | "}
 			<Link to="/about" activeClassName="active">About</Link>
 		</nav>
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,12 @@ import { Route, IndexRoute } from 'react-router'
 import App from './components/App'
 import HomePage from './components/home/HomePage'
 import AboutPage from './components/about/AboutPage'
-// import CoursesPage from './components/course/CoursesPage'
-// import ManageCoursePage from './components/course/ManageCoursePage' //eslint-disable-line import/no-named-as-default
+import CoursesPage from './components/course/CoursesPage'
 
 export default (
 	<Route path="/" component={ App }>
 		<IndexRoute component={ HomePage } />
+		<Route path="courses" component={ CoursesPage } />
 		<Route path="about" component={ AboutPage } />
 	</Route>
 )
