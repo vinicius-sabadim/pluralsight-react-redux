cf16e360ab7b87ca17fb8bea15cac5025a880edb
vinicius-sabadim <vinicius.sabadim@gmail.com>
2016-07-21 23:46:01 -0300

    Changing the implicit dispatch to mapDispatchToProps

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -21,7 +21,7 @@ class CoursesPage extends React.Component {
 	}
 
 	onClickSave() {
-		this.props.dispatch(courseActions.createCourse(this.state.course))
+		this.props.createCourse(this.state.course)
 	}
 
 	courseRow(course, index) {
@@ -48,8 +48,8 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes = {
-	dispatch: PropTypes.func.isRequired,
-	courses: PropTypes.array.isRequired
+	courses: PropTypes.array.isRequired,
+	createCourse: PropTypes.func.isRequired
 }
 
 function mapStateToProps(state, ownProps) {
@@ -58,4 +58,10 @@ function mapStateToProps(state, ownProps) {
 	}
 }
 
-export default connect(mapStateToProps)(CoursesPage)
+function mapDispatchToProps(dispatch) {
+	return {
+		createCourse: course => dispatch(courseActions.createCourse(course))
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage)
