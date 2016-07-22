4eb258d7d268d19022ffa2482a3d7b36d753c1ee
vinicius-sabadim <vinicius.sabadim@gmail.com>
2016-07-21 23:36:46 -0300

    Implementing the redux pattern

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
-// import configureStore from './store/configureStore'
-// import {Provider} from 'react-redux'
+import configureStore from './store/configureStore'
+import { Provider } from 'react-redux'
 import { Router, browserHistory } from 'react-router'
 import routes from './routes'
 // import {loadCourses} from './actions/courseActions'
@@ -11,11 +11,13 @@ import './styles/styles.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 // import '../node_modules/toastr/build/toastr.min.css'
 
-// const store = configureStore()
+const store = configureStore()
 // store.dispatch(loadCourses())
 // store.dispatch(loadAuthors())
 
 render(
-	<Router history={ browserHistory } routes={ routes } />,
+	<Provider store={ store }>
+		<Router history={ browserHistory } routes={ routes } />
+	</Provider>,
 	document.getElementById('app')
 )
