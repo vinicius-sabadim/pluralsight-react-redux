17ffa8be717c4948cb1f501b54d0a8a2802c06ec
vinicius-sabadim <vinicius.sabadim@gmail.com>
2016-07-31 19:50:02 -0300

    Adding a throw error

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -41,6 +41,7 @@ export function saveCourse(course) {
 			course.id ? dispatch(updateCourseSuccess(savedCourse)) : dispatch(createCourseSuccess(savedCourse))
 		}).catch(error => {
 			dispatch(ajaxCallError(error))
+			throw(error)
 		})
 	}
 }
